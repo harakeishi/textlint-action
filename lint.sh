f455df8ccc37bd69df1ea3a2647111f20c49489d
harakeishi <hara.for.work@gmail.com>
2023-08-19 16:34:59 +0900

    fix

--- lint.sh
+++ lint.sh
@@ -2,7 +2,7 @@
 
 cd "${GITHUB_ACTION_PATH}" || exit
 npm ci
-if [ -z "${INPUT_CONFIG}" ]; then
+if [ -x "${INPUT_CONFIG}" ]; then
   cp ${INPUT_CONFIG} .textlintrc
 fi
 npx textlint ${INPUT_TARGET}
